4832980999e5b338548fb7cc37498d22b179cba9
shivani-sri-001 <shivanisrir07@gmail.com>
2025-06-22 09:50:11 +0530

    java exercise problems

---
+++ New.java
@@ -0,0 +1,24 @@
+class New
+{
+
+   public static void main(String[] arg)
+      {
+
+         byte a=10;
+         short b=30;
+         int c=120;
+         long d=13247287;
+         float e=76.2f;
+         double f=87.79273781368d;
+         boolean g=true;
+         char h='%';
+         System.out.println(a);
+         System.out.println(b);
+        
+
+         
+     }
+
+
+
+}
