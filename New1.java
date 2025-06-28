4896a40bf5f95d81666504a8dcd00553da0f211a
shivani-sri-001 <shivanisrir07@gmail.com>
2025-06-28 11:06:50 +0530

    while loop exercise

---
+++ New1.java
@@ -0,0 +1,26 @@
+class New1
+
+{
+      public static void main(String[] arg)
+   
+       {
+          
+         int a=1;
+         int s=0;
+     
+         while (a<=10)
+       
+           {
+   
+              s=s+a;
+              a++; 
+        
+           }
+   
+         {      
+            System.out.println(s);
+         }
+        
+        }     
+
+}
