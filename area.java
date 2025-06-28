2ef10d400f57e067d84c4ab8e57b2f4bdb3aaa75
shivani-sri-001 <shivanisrir07@gmail.com>
2025-06-28 10:41:49 +0530

    exercises

---
+++ area.java
@@ -0,0 +1,20 @@
+public class Main 
+{
+    public static void main(String[] args) 
+   {
+        double radius = 7.0; 
+        double area = Math.PI * radius * radius;
+
+        System.out.println("Area of the circle: " + area);
+
+        double base = 10.0;          
+        double height = 5.0;  
+
+        double area = (base * height) / 2;
+
+        System.out.println("Area of the triangle: " + area);
+    }
+
+
+    
+}
