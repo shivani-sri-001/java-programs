1dda979178a950882f0b297abf804ccb0dde511a
shivani-sri-001 <shivanisrir07@gmail.com>
2025-06-28 10:43:08 +0530

    java exercises

---
+++ paragraph.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to our website!\nWe offer a wide range of products to meet your needs.\nFeel free to browse and contact us for more information.\nHappy shopping!");
+    }
+}
---
+++ temperature.java
@@ -0,0 +1,10 @@
+public class Main
+ {
+    public static void main(String[] args)
+   {
+        double celsius = 25.0; 
+        double fahrenheit = (celsius * 1.8) + 32;
+
+        System.out.println("Temperature in Fahrenheit: " + fahrenheit);
+    }
+ }
